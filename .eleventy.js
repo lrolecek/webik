845b915d2b9dd5e26a30155c91e3c6a94936db06
Luděk Roleček <ludek@rolecek.cz>
2020-12-08 20:07:29 +0100

    kontaktni formular

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 
 	// Zkopírovat `css/` to `_site/css/`
-	eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("css");
+
+	eleventyConfig.addPassthroughCopy("js");
 
   return {
     // možné formáty šablon
